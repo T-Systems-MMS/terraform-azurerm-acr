ba9f6c49089eb18ab6ddbda1c757613f0d92a03f
mima <Michaela.Mattes@t-systems.com>
2021-12-15 13:45:58 +0100

    add main

--- versions.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "registry.terraform.io/hashicorp/azurerm"
-      version = ">=2.19.0"
-    }
-  }
-  required_version = ">=0.12"
-}
