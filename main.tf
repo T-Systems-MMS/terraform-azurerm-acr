63f0e4c293e533a6c899b3b4bbfffd505baf3054
mima <Michaela.Mattes@t-systems.com>
2022-02-10 13:14:19 +0100

    add more arguments

--- main.tf
+++ main.tf
@@ -13,6 +13,15 @@ resource "azurerm_container_registry" "acr" {
   resource_group_name = local.acr[each.key].resource_group_name
   sku                 = local.acr[each.key].sku
   admin_enabled       = local.acr[each.key].admin_enabled
+  public_network_access_enabled = local.acr[each.key].public_network_access_enabled
+
+  retention_policy {
+    days = local.acr[each.key].retention_policy.days
+    enabled = local.acr[each.key].retention_policy.enabled
+  }
+  trust_policy {
+    enabled = local.acr[each.key].trust_policy.enabled
+  }
 
   tags = local.acr[each.key].tags
 
--- variables.tf
+++ variables.tf
@@ -11,14 +11,37 @@ locals {
       name          = ""
       sku           = "Basic"
       admin_enabled = true
+      public_network_access_enabled = true
+      quarantine_policy_enabled = false
+      zone_redundancy_enabled = false
+      network_rule_bypass_option = "AzureServices"
+      anonymous_pull_enabled = false
+      data_endpoint_enabled         = false
+      retention_policy = {
+        days = 7
+        enabled = false
+      }
+      trust_policy = {
+        enabled = false
+      }
       tags          = {}
     }
   }
 
   # compare and merge custom and default values
+  acr_values = {
+    for acr in keys(var.acr) :
+    acr => merge(local.default.acr, var.acr[acr])
+  }
   # merge all custom and default values
   acr = {
     for acr in keys(var.acr) :
-    acr => merge(local.default.acr, var.acr[acr])
+    acr => merge(
+      local.acr_values[acr],
+      {
+        for config in ["retention_policy","trust_policy"] :
+        config => merge(local.default.acr[config], local.acr_values[acr][config])
+      }
+    )
   }
 }
